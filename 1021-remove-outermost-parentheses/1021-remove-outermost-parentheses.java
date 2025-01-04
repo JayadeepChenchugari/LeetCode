394ffc376fd107f578ef17150623050577f5609b
Chenchugari Jayadeep <126348368+JayadeepChenchugari@users.noreply.github.com>
2025-01-04 14:31:46 +0530

    Time: 2 ms (98.74%), Space: 42.1 MB (72.18%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder sb=new StringBuilder();
+        int count=0;
+        for(char c:s.toCharArray()){
+            if(c=='('){
+                if(count!=0){
+                    sb.append(c);
+                }
+                count++;
+            }
+            else{
+                if(count>1){
+                    sb.append(c);
+                }
+                count--;
+            }
+        }
+        return sb.toString();
+    }
+}
