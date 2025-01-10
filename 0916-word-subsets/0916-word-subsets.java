ea2d9ae1a373d7dbdbcb192103c6e480035e6550
Chenchugari Jayadeep <126348368+JayadeepChenchugari@users.noreply.github.com>
2025-01-11 00:18:31 +0530

    Time: 13 ms (81.4%), Space: 52.5 MB (99.42%) - LeetHub

--- 0916-word-subsets/0916-word-subsets.java
+++ 0916-word-subsets/0916-word-subsets.java
@@ -1,38 +1,33 @@
 class Solution {
     public List<String> wordSubsets(String[] words1, String[] words2) {
-        int[] maxCharFreq = new int[26];
-        int[] tempCharFreq = new int[26];
-
-        for (String word : words2) {
-            Arrays.fill(tempCharFreq, 0);
-            for (char ch : word.toCharArray()) {
-                tempCharFreq[ch - 'a']++;
+        int[] tempCharfreq=new int[26];
+        int[] maxCharfreq=new int[26];
+        for(String word:words2){
+            Arrays.fill(tempCharfreq,0);
+            for(char c:word.toCharArray()){
+                tempCharfreq[c-'a']++;
             }
-            for (int i = 0; i < 26; ++i) {
-                maxCharFreq[i] = Math.max(maxCharFreq[i], tempCharFreq[i]);
+            for(int i=0;i<26;i++){
+                maxCharfreq[i]=Math.max(maxCharfreq[i],tempCharfreq[i]);
             }
         }
-
-        List<String> universalWords = new ArrayList<>();
-
-        for (String word : words1) {
-            Arrays.fill(tempCharFreq, 0);
-            for (char ch : word.toCharArray()) {
-                tempCharFreq[ch - 'a']++;
+        List<String> universal=new ArrayList<>();
+        for(String word:words1){
+            Arrays.fill(tempCharfreq,0);
+            for(char c:word.toCharArray()){
+                tempCharfreq[c-'a']++;
             }
-
-            boolean isUniversal = true;
-            for (int i = 0; i < 26; ++i) {
-                if (maxCharFreq[i] > tempCharFreq[i]) {
-                    isUniversal = false;
+            boolean issubset=true;
+            for(int i=0;i<26;i++){
+                if(maxCharfreq[i]>tempCharfreq[i]){
+                    issubset= false;
                     break;
                 }
             }
-            if (isUniversal) {
-                universalWords.add(word);
+            if(issubset){
+                universal.add(word);
             }
         }
-
-        return universalWords;
+        return universal;
     }
 }
