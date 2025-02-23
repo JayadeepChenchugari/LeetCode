d13c9fdf912b390fc685a8b234a1afdbbe4ed40d
Chenchugari Jayadeep <126348368+JayadeepChenchugari@users.noreply.github.com>
2025-02-23 22:13:08 +0530

    Time: 6 ms (50.65%), Space: 42.3 MB (79.23%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.java
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.java
@@ -1,22 +1,21 @@
-import java.util.*;
 class Solution {
     public long pickGifts(int[] gifts, int k) {
         while(k>0){
+            int n=gifts.length;
             int max=Integer.MIN_VALUE;
             int index=-1;
-            for(int i=0;i<gifts.length;i++){
+            for(int i=0;i<n;i++){
                 if(gifts[i]>max){
                     max=gifts[i];
                     index=i;
                 }
             }
             gifts[index]=(int)Math.floor(Math.sqrt(max));
-            
             k--;
         }
         long sum=0;
-        for(int gift:gifts){
-            sum+=gift;
+        for(int num:gifts){
+            sum+=num;
         }
         return sum;
     }
