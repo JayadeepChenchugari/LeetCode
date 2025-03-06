66db2272db3700dcb49fc8041483a9b4e3241776
Chenchugari Jayadeep <126348368+JayadeepChenchugari@users.noreply.github.com>
2025-03-06 23:00:04 +0530

    Time: 1 ms (65.64%), Space: 41.6 MB (62.44%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -6,7 +6,7 @@ class Solution {
         StringBuilder sb=new StringBuilder();
         for(int i=0;i<Math.min(s1.length(),s2.length());i++){
             if(s1.charAt(i)!=s2.charAt(i)){
-                break;
+                 return sb.toString();
             }
             sb.append(s1.charAt(i));
         }
