b9f35b803f35a3b8e7a75f7c09971fc1382ad05d
Chenchugari Jayadeep <126348368+JayadeepChenchugari@users.noreply.github.com>
2025-03-10 23:30:16 +0530

    Time: 4 ms (18.73%), Space: 42.7 MB (43.38%) - LeetHub

--- 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.java
+++ 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.java
@@ -1,22 +1,22 @@
 class Solution {
     public int countPrefixSuffixPairs(String[] words) {
-        int n=words.length;
         int count=0;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(isPrefixSuffix(words[i],words[j])){
+        for(int i=0;i<words.length;i++){
+            for(int j=0;j<words.length;j++){
+                if(i==j){
+                    continue;
+                }
+                if(isPrefixAndSuffix(words[i],words[j])&& i<j){
                     count++;
                 }
             }
         }
         return count;
     }
-    public boolean isPrefixSuffix(String s1,String s2){
-        int l=s1.length();
-        int m=s2.length();
-        if(l>m){
-            return  false;
+    public boolean isPrefixAndSuffix(String s1,String s2){
+        if(s2.startsWith(s1)&& s2.endsWith(s1)){
+            return true;
         }
-        return s2.startsWith(s1) && s2.endsWith(s1);
+        return false;
     }
 }
