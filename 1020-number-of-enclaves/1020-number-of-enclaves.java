a827f056b0f9c9b32955415bdeea56fc3f4fa251
Chenchugari Jayadeep <126348368+JayadeepChenchugari@users.noreply.github.com>
2025-12-17 16:21:41 +0530

    Time: 9 ms (56.37%), Space: 55.6 MB (72.49%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -3,35 +3,38 @@ class Solution {
         int n=grid.length;
         int m=grid[0].length;
         int[][] vis=new int[n][m];
+        Queue<int[]> q=new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if((i==0||j==0||i==n-1||j==m-1) &&grid[i][j]==1){
-                    dfs(grid,vis,i,j);
+                if(i==0 || i==n-1 || j==0 || j==m-1){
+                    if(grid[i][j]==1){
+                        q.add(new int[]{i,j});
+                        vis[i][j]=1;
+                    }
                 }
             }
         }
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1 && vis[i][j]==0){
-                    cnt++;
+        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+        while(!q.isEmpty()){
+            int[] curr=q.poll();
+            int r=curr[0] , c=curr[1];
+            for(int[] d:dir){
+                int row=r+d[0];
+                int col=c+d[1];
+                if(row>=0 && row<n && col>=0 && col<m && vis[row][col]==0 && grid[row][col]==1){
+                    vis[row][col]=1;
+                    q.add(new int[]{row , col});
                 }
             }
         }
-        return cnt;
-    }
-    public void dfs(int[][] grid,int[][] vis,int row,int col){
-        int n=grid.length;
-        int m=grid[0].length;
-        int[] delrow={-1,0,1,0};
-        int[] delcol={0,1,0,-1};
-        vis[row][col]=1;
-        for(int i=0;i<4;i++){
-            int nrow=row+delrow[i];
-            int ncol=col+delcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&grid[nrow][ncol]==1 && vis[nrow][ncol]==0){
-                dfs(grid,vis,nrow,ncol);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==0 && grid[i][j]==1){
+                    ans++;
+                }
             }
         }
+        return ans;
     }
 }
