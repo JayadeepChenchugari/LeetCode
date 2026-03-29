24db2664abc4199a829b1ccc9bff01ccb9af73b8
Chenchugari Jayadeep <126348368+JayadeepChenchugari@users.noreply.github.com>
2026-03-29 12:16:05 +0530

    Time: 11 ms (17.34%), Space: 68.2 MB (5.03%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -3,38 +3,35 @@ class Solution {
         int n=grid.length;
         int m=grid[0].length;
         int[][] vis=new int[n][m];
-        Queue<int[]> q=new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(i==0 || i==n-1 || j==0 || j==m-1){
-                    if(grid[i][j]==1){
-                        q.add(new int[]{i,j});
-                        vis[i][j]=1;
-                    }
+                if((i==0||j==0||i==n-1||j==m-1) &&grid[i][j]==1){
+                    dfs(grid,vis,i,j);
                 }
             }
         }
-        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
-        while(!q.isEmpty()){
-            int[] curr=q.poll();
-            int r=curr[0] , c=curr[1];
-            for(int[] d:dir){
-                int row=r+d[0];
-                int col=c+d[1];
-                if(row>=0 && row<n && col>=0 && col<m && vis[row][col]==0 && grid[row][col]==1){
-                    vis[row][col]=1;
-                    q.add(new int[]{row , col});
-                }
-            }
-        }
-        int ans=0;
+        int cnt=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis[i][j]==0 && grid[i][j]==1){
-                    ans++;
+                if(grid[i][j]==1 && vis[i][j]==0){
+                    cnt++;
                 }
             }
         }
-        return ans;
+        return cnt;
+    }
+    public void dfs(int[][] grid,int[][] vis,int row,int col){
+        int n=grid.length;
+        int m=grid[0].length;
+        int[] delrow={-1,0,1,0};
+        int[] delcol={0,1,0,-1};
+        vis[row][col]=1;
+        for(int i=0;i<4;i++){
+            int nrow=row+delrow[i];
+            int ncol=col+delcol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&grid[nrow][ncol]==1 && vis[nrow][ncol]==0){
+                dfs(grid,vis,nrow,ncol);
+            }
+        }
     }
 }
