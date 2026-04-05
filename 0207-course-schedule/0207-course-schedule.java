0119c5df3fa0f7ef610ec8a59ba6b3d8b59a93e2
Chenchugari Jayadeep <126348368+JayadeepChenchugari@users.noreply.github.com>
2026-04-05 22:09:18 +0530

    Time: 6 ms (74.2%), Space: 47.6 MB (6.95%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,40 +1,39 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> graph=new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
         for(int i=0;i<numCourses;i++){
-            graph.add(new ArrayList<>());
-        }
-        int m=prerequisites.length;
-        for(int i=0;i<m;i++){
-            graph.get(prerequisites[i][0]).add(prerequisites[i][1]);
+            adj.add(new ArrayList<>());
         }
         int[] indegree=new int[numCourses];
+        for(int i=0;i<prerequisites.length;i++){
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
         for(int i=0;i<numCourses;i++){
-            for(int it:graph.get(i)){
+            for(int it:adj.get(i)){
                 indegree[it]++;
             }
         }
         Queue<Integer> q=new LinkedList<>();
-        for(int i=0;i<indegree.length;i++){
+        for(int i=0;i<numCourses;i++){
             if(indegree[i]==0){
                 q.add(i);
             }
         }
-        List<Integer> topo=new ArrayList<>();
+        List<Integer> ans=new ArrayList<>();
         while(!q.isEmpty()){
             int node=q.peek();
             q.remove();
-            topo.add(node);
-            for(int it:graph.get(node)){
+            ans.add(node);
+            for(int it:adj.get(node)){
                 indegree[it]--;
                 if(indegree[it]==0){
                     q.add(it);
                 }
             }
         }
-        if(topo.size()==numCourses){
-            return true;
+        if(ans.size()!=numCourses){
+            return false;
         }
-        return false;
+        return true;
     }
 }
