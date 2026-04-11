af0c48fc3e281f229ccc9b1eadee1e5264c84b5f
Chenchugari Jayadeep <126348368+JayadeepChenchugari@users.noreply.github.com>
2026-04-11 20:45:53 +0530

    Time: 93 ms (35.84%), Space: 48.6 MB (36.77%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,22 +1,20 @@
-
 class Pair{
     String first;
     int second;
-    Pair(String _first , int _second){
-        this.first=_first;
-        this.second=_second;
+    Pair(String first , int second){
+        this.first=first;
+        this.second=second;
     }
 }
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-         Queue<Pair> q=new LinkedList<>();
-         q.add(new Pair(beginWord , 1));
-         Set<String> set=new HashSet<String>();
-         for(int i=0;i<wordList.size();i++){
+        Queue<Pair> q=new LinkedList<>();
+        Set<String> set=new HashSet<>();
+        for(int i=0;i<wordList.size();i++){
             set.add(wordList.get(i));
-         }
-         set.remove(beginWord);
-         while(!q.isEmpty()){
+        }
+        q.add(new Pair(beginWord,1));
+        while(!q.isEmpty()){
             String word=q.peek().first;
             int steps=q.peek().second;
             q.remove();
@@ -25,16 +23,16 @@ class Solution {
             }
             for(int i=0;i<word.length();i++){
                 for(char c='a';c<='z';c++){
-                char newWord[]=word.toCharArray();
-                newWord[i]=c;
-                String replacedWord=new String(newWord);
-                if(set.contains(replacedWord)==true){
-                    set.remove(replacedWord);
-                    q.add(new Pair(replacedWord, steps+1));
+                    char[] replacedChar=word.toCharArray();
+                    replacedChar[i]=c;
+                    String replacedWord=new String(replacedChar);
+                    if(set.contains(replacedWord)==true){
+                        set.remove(replacedWord);
+                        q.add(new Pair(replacedWord,steps+1));
+                    }
                 }
             }
-            }
-         }
-         return 0;
+        }
+        return 0;
     }
 }
