3affcfaa628d030389efdad39a79f18938450638
Chenchugari Jayadeep <126348368+JayadeepChenchugari@users.noreply.github.com>
2026-05-02 10:52:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,26 +3,18 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         int[][] dp=new int[m][n];
-        for(int[] row:dp){
-            Arrays.fill(row ,-1);
+        dp[0][0]=grid[0][0];
+        for(int i=1;i<m ; i++){
+            dp[i][0]=grid[i][0] + dp[i-1][0];
         }
-        int ans=sumPath(0,0,grid , dp);
-        return ans;
-    }
-    public int sumPath(int i , int j ,int[][] grid  , int[][] dp){
-        int m=grid.length;
-        int n=grid[0].length;
-        if(i>=m || j>=n){
-            return Integer.MAX_VALUE;
+        for(int j=1; j<n ;j++){
+            dp[0][j]=grid[0][j] + dp[0][j-1];
         }
-        if(i==m-1 && j==n-1){
-            return grid[i][j];
+        for(int i=1;i<m;i++){
+            for(int j=1 ; j<n ; j++){
+                dp[i][j]=grid[i][j] + Math.min(dp[i-1][j] , dp[i][j-1]);
+            }
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int left = sumPath( i+1 , j , grid , dp);
-        int right = sumPath(i ,j+1  ,grid , dp);
-        return dp[i][j] = grid[i][j] + Math.min(left , right);
+        return dp[m-1][n-1];
     }
 }
